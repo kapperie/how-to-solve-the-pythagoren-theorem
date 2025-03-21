8ad19de97a6bad1af1506ecb624fd7d00b12d1c8
website maker <cherrychomperxo@gmail.com>
2025-03-21 03:02:03 -0400

    Add files via upload

---
+++ the pythagoren thorem solver.py
@@ -0,0 +1,11 @@
+# a is a short side
+# b is another short side
+# c is the hypotenuse
+a = int(input('what is A?: '))
+
+b = int(input('what is B?: '))
+
+c = (a**2) + (b**2) ** 0.5
+print("the hypotenuse is")
+
+print(c)
